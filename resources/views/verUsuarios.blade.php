f980b508886385ed83c11db3a7a7eb196cb32ec2
SantiagoSosaM <santiago.sosa.m@estudiantes.utec.edu.uy>
2023-05-30 15:38:01 -0300

    placeholder

--- resources/views/verUsuarios.blade.php
+++ resources/views/verUsuarios.blade.php
@@ -3,11 +3,36 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>List of Users</title>
-	<!-- Add your stylesheets here -->
-	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title>Mi página con Bootstrap</title>
+	<!-- Agregamos los estilos de Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/styleMisCursos.css')}}">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+		<a class="navbar-brand" href="{{route('inicio')}}">
+			<img src="{{asset('images/1.png')}}" width="150" height="50" class="d-inline-block align-top" alt="Logo de Mi Página">
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="{{route('inicio')}}">Inicio</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="{{route('miscursos')}}">Mis Cursos<span class="sr-only">(current)</span></a>
+				</li>
+			</ul>
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="{{route('logout')}}">Cerrar Sesión</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
     <div class="container">
         <h1>Lista de Usuarios</h1>
         <table class="table">
